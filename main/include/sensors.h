43c8af3aa8fe84d15742d36c6ca3dc28d606de3b
Preston <prestonmeek6@gmail.com>
2025-10-10 19:26:24 +0000

    Got timers working

--- main/include/sensors.h
+++ main/include/sensors.h
@@ -5,10 +5,17 @@
 #include "freertos/task.h"
 #include "freertos/timers.h"
 
+#include <array>
+
 constexpr uint32_t UWB_ID = 0;
 constexpr uint32_t IMU_ID = 1;
+constexpr uint32_t SENSOR_COUNT = 2;
 
+// TODO: consider making this a class per sensor where each has a single StaticTimer_t
+// can avoid all this weird extern stuff then
 namespace Sensors {
+    extern StaticTimer_t sensor_buffers[SENSOR_COUNT];
+    
     void spin(void* context);
     void registerSensorTimer(TimerHandle_t timer);
     void registerSensorTaskHandle(TaskHandle_t taskHandle);
--- main/src/main.cpp
+++ main/src/main.cpp
@@ -4,6 +4,8 @@
 #include "ros.h"
 #include "sensors.h"
 
+#include "freertos/FreeRTOS.h"
+
 // TODO: make templated and move to consumer.h?
 // TODO: make struct so we can pass multiple parameters
 
@@ -34,17 +36,17 @@ extern "C" void app_main(void) {
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
 
-    HW::MotorDriver motor_driver;
+    /*HW::MotorDriver motor_driver;
     Queue<Consumer::MessageTag, Consumer::MessageBody, Consumer::CONSUMER_QUEUE_SIZE> queue;
 
     ConsumerTaskData consumerTaskData {
         motor_driver,
         queue
-    };
+    };*/
 
     log("Hello world!");
 
-    xTaskCreate(
+    /*xTaskCreate(
         ROS::spin,
         "uros_task",
         4096, // TODO: see https://github.com/micro-ROS/micro_ros_espidf_component/blob/cd1da2b3d7d73f48743a2c42ac0e915cd751bb74/examples/int32_publisher/main/main.c#L105
@@ -60,10 +62,9 @@ extern "C" void app_main(void) {
         (void*)&consumerTaskData,
         configMAX_PRIORITIES - 1,
         NULL
-    );
+    );*/
 
     TaskHandle_t sensorTaskHandle;
-
     xTaskCreate(
         Sensors::spin,
         "sensors_task",
@@ -72,14 +73,15 @@ extern "C" void app_main(void) {
         configMAX_PRIORITIES - 1,
         &sensorTaskHandle
     );
-
     Sensors::registerSensorTaskHandle(sensorTaskHandle);
+    log("Registered handle");
 
-    xTimerCreate(
+    TimerHandle_t test = xTimerCreate(
         "uwb_timer",
-        pdMS_TO_TICKS(100), // 100 ms period. TODO: change?
+        pdMS_TO_TICKS(1000), // 100 ms period. TODO: change?
         pdTRUE,
         (void*)UWB_ID,
         Sensors::registerSensorTimer
     );
+    xTimerStart(test, 0);
 }
