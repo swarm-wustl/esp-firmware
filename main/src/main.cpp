084f26a585aa161cd7a5501d6f68ff28e8e74d92
Preston <prestonmeek6@gmail.com>
2026-09-05 13:37:29 -0500

    Remove stale files

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -3,7 +3,6 @@
 #include "hardware.h"
 #include "queue.h"
 #include "ros.h"
-#include "sensor.h"
 
 #include "freertos/FreeRTOS.h"
 #include <memory>
@@ -96,21 +95,4 @@ extern "C" void app_main(void) {
 
   xTaskCreate(consumerTaskWrapper, "consumer_task", 4096,
               (void *)&consumerTaskData, configMAX_PRIORITIES - 1, NULL);
-
-  // Create sensor task and register the task handle for the timers
-  // TODO: wrap this in a function?
-  /*TaskHandle_t sensorTaskHandle;
-  xTaskCreate(
-      Sensor::spin,
-      "sensors_task",
-      4096,
-      NULL,
-      configMAX_PRIORITIES - 1,
-      &sensorTaskHandle
-  );
-  Sensor::registerSensorTaskHandle(sensorTaskHandle);
-  log("Registered handle");
-
-  // TODO: make constant time
-  Sensor uwb(UWB_ID, "uwb_sensor", 1000);*/
 }
